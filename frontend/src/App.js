03caa6edfc92472628aeb393ebeacdd015fda258
Dhruv Rajvanshi <100338544+Dhruv-Rajvanshi@users.noreply.github.com>
2025-02-24 16:05:21 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -79,7 +79,7 @@ class App extends React.Component {
 
     var url = 'http://127.0.0.1:8000/api/task-create/'
 
-    if(this.state.editing == true){
+    if(this.state.editing === true){
       url = `http://127.0.0.1:8000/api/task-update/${ this.state.activeItem.id}/`
       this.setState({
         editing:false
@@ -184,7 +184,7 @@ class App extends React.Component {
 
                             <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
 
-                                {task.completed == false ? (
+                                {task.completed === false ? (
                                     <span>{task.title}</span>
 
                                   ) : (
